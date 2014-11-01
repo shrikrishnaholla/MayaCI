ad01ffcd98ece56f14661d42e31cef1078f08396
Shrikrishna Holla <shrikrishna@freshdesk.com>
2014-11-01 14:47:36 +0530

    Committing random stuff

--- controllers/build.js
+++
@@ -1,5 +0,0 @@
-exports.build = function(req, res) {
-	var commit_info = JSON.parse(req.body.payload);
-	console.log(JSON.stringify(commit_info));
-	res.json(commit_info);
-};
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var util = require('util');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* Web hook handler */
+router.post('/webhook', function(req, res) {
+  var event_type = req.headers["x-gitHub-event"]
+  var delivery_guid = req.headers["x-gitHub-delivery"]
+  var signature = req.headers["x-hub-signature"]
+  var body = req.body;
+  console.log('Event type: ' + event_type);
+  console.log('Delivery Guid: ' + delivery_guid);
+  console.log('Signature: ' + signature);
+  console.log('body: ' + util.inspect(body));
+  res.send('OK');
+});
+
+module.exports = router;
